60de6a9e4fc19863257165127d8c6423cbb5240f
Robin <midouesterner@gmail.com>
2022-07-02 11:26:47 -0500

    clean up fixer assertion helpers

--- src/Fixer.ts
+++ src/Fixer.ts
@@ -3,14 +3,14 @@ import { OnProxyServer } from "./OnProxyServer";
 
 export type Fixer = Partial<OnProxyClient> & Partial<OnProxyServer>;
 
-export function isClientFix(
-  fixer: Fixer
-): fixer is OnProxyClient & Partial<OnProxyServer> {
+type ClientFix = OnProxyClient & Partial<OnProxyServer>;
+
+export function isClientFix(fixer: Fixer): fixer is ClientFix {
   return fixer.onProxyClient !== undefined;
 }
 
-export function isServerFix(
-  fixer: Fixer
-): fixer is OnProxyServer & Partial<OnProxyClient> {
+type ServerFix = OnProxyServer & Partial<OnProxyClient>;
+
+export function isServerFix(fixer: Fixer): fixer is ServerFix {
   return fixer.onProxyServer !== undefined;
 }

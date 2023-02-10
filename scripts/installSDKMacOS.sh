b80ecabf88475aa47eb986eda458a33042d325d0
Robin <midouesterner@gmail.com>
2023-02-10 06:48:48 -0600

    macOS install script can download sdk 1.13

--- scripts/installSDKMacOS.sh
+++ scripts/installSDKMacOS.sh
@@ -2,15 +2,23 @@
 
 set -euxo pipefail
 
-VERSION=1.12.3
+VERSION=${1:-1.12.3}
 PLAYDATE_SDK=PlaydateSDK-${VERSION}
 
-mkdir tmp
-cd tmp
+rm -rf tmp/${VERSION}
+mkdir -p tmp/${VERSION}
+cd tmp/${VERSION}
 
 wget https://download.panic.com/playdate_sdk/${PLAYDATE_SDK}.zip
 unzip ${PLAYDATE_SDK}.zip
-xar -xf ${PLAYDATE_SDK}.pkg
-cat PlaydateSDK.pkg/Payload | gunzip -dc | cpio -i
-rm -rf ../fixtures/${PLAYDATE_SDK}
-mv PlaydateSDK ../fixtures/${PLAYDATE_SDK}
+
+PKG_FILE="${PLAYDATE_SDK}.pkg"
+if [[ -f "PlaydateSDK.pkg" ]]; then
+    PKG_FILE="PlaydateSDK.pkg"
+fi
+
+mkdir out
+xar -xf "${PKG_FILE}" -C out
+cat out/PlaydateSDK.pkg/Payload | gunzip -dc | cpio -i
+rm -rf ../../fixtures/${PLAYDATE_SDK}
+mv PlaydateSDK ../../fixtures/${PLAYDATE_SDK}
